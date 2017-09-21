6204704ca899345998a989e2fd3c0566732ae296
Anthony Powers <tehraven@gmail.com>
2017-09-20 19:09:37 -0700

    PHP config changes

--- up.bat
+++ up.bat
@@ -4,7 +4,6 @@ docker build -t tehraven/lbfb-discord-bot .
 
 FOR /f "tokens=*" %%i IN ('docker ps -a -q') DO docker rm -f %%i
 
-docker run -d --name web -p 80:80 -t tehraven/lbfb-discord-bot
-  
+docker run -d --name web -e DNSMASQ_SERVERS="8.8.8.8,8.8.4.4" -e DNSMASQ_DEFAULT=0 -t tehraven/lbfb-discord-bot
 docker ps
-docker logs -f web
+docker logs web
